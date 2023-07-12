12d32135b4b5582b67df0ae3abb9e8f8caf0f568
Jeremy Lille <jeremtruc31@hotmail.fr>
2023-07-12 15:50:25 +0200

    Update flag

--- basketball-statistics-location-exposition/src/main/java/fr/bsm/location/exposition/util/CityDtoMapper.java
+++ basketball-statistics-location-exposition/src/main/java/fr/bsm/location/exposition/util/CityDtoMapper.java
@@ -1,6 +1,8 @@
 package fr.bsm.location.exposition.util;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import fr.bsm.location.domain.common.entity.city.CitiesEntity;
 import fr.bsm.location.domain.common.entity.city.CityEntity;
@@ -12,10 +14,17 @@ import fr.bsm.location.exposition.dto.CityRequestDto;
 @Mapper(componentModel = "spring", uses = {})
 public interface CityDtoMapper {
 
+	@Mapping(source = "country.codeiso2", target = "country.flagurl", qualifiedByName = "buildFlag")
 	CityDto entityToDto(CityEntity cityEntity);
+	
 	
 	CityEntity dtoToEntity(CityRequestDto cityRequestDto);
 	
 	CitiesDto entityToCitiesDto(CitiesEntity citiesEntity);
+	
+	@Named("buildFlag")
+	public static String buildFlag(String codeiso2) {
+		return "http://paulemignoni.alwaysdata.net/flags/16x16/"+codeiso2.toLowerCase()+".png";
+	}
 	
 }
