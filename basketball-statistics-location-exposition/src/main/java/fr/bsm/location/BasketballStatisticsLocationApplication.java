3004e640866386cbc4c5e51ebc1e033b9f8ff0cf
Jeremy Lille <jeremtruc31@hotmail.fr>
2023-08-09 16:30:57 +0200

    add dwagger generation

--- basketball-statistics-location-exposition/src/main/java/fr/bsm/location/BasketballStatisticsLocationApplication.java
+++ basketball-statistics-location-exposition/src/main/java/fr/bsm/location/BasketballStatisticsLocationApplication.java
@@ -3,13 +3,20 @@ package fr.bsm.location;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import lombok.extern.slf4j.Slf4j;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @Slf4j
+@EnableSwagger2
 public class BasketballStatisticsLocationApplication {
 
   /**
@@ -51,4 +58,10 @@ public class BasketballStatisticsLocationApplication {
         hostAddress,
         serverPort, contextPath, env.getActiveProfiles());
   }
+  
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("fr.bsm")).build();
+	}
 }
